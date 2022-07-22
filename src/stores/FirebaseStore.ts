340c93ba02c461efd5e84fa2ba2399c3b248acc4
Maarten van Wersch <maartenvanwersch@hotmail.com>
2022-07-22 15:11:07 +0200

    RootStore generic type added

--- src/stores/FirebaseStore.ts
+++ src/stores/FirebaseStore.ts
@@ -2,12 +2,12 @@ import { FirebxRootStore } from "../@firebx-types";
 import { FirebaseApp } from "firebase/app";
 import { Firestore, getFirestore } from "firebase/firestore";
 
-export class FirebaseStore<RootStore extends FirebxRootStore> {
-  readonly rootStore: RootStore;
+export class FirebaseStore {
+  readonly rootStore: FirebxRootStore;
 
   readonly firestore: Firestore;
 
-  constructor({ rootStore, firebaseApp }: { rootStore: RootStore, firebaseApp: FirebaseApp }) {
+  constructor({ rootStore, firebaseApp }: { rootStore: FirebxRootStore, firebaseApp: FirebaseApp }) {
     this.rootStore = rootStore;
     this.firestore = getFirestore(firebaseApp);
   }
