cc5d60ac29f01f211387efb3f930dcfe310a5742
Maarten van Wersch <maartenvanwersch@hotmail.com>
2022-07-22 12:33:31 +0200

    CollectionStore.ts constructor bug fixed

--- src/stores/CollectionStore.ts
+++ src/stores/CollectionStore.ts
@@ -3,7 +3,7 @@ import {WithUid} from "../@firebx-types";
 import {FetchServiceProps} from "../@firebx-types/service";
 
 export class CollectionStore<Type extends WithUid> extends AbstractCollectionStore<Type> {
-  protected constructor({ rootStore, collectionId }: FetchServiceProps) {
+  constructor({ rootStore, collectionId }: FetchServiceProps) {
     super({ rootStore, collectionId });
   }
 }
