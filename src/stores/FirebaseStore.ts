3f55c0bd81196dab1d6d237096487824a2ad0582
Maarten van Wersch <maartenvanwersch@hotmail.com>
2022-07-23 12:43:07 +0200

    FirebaseStore.ts updated

--- src/stores/FirebaseStore.ts
+++ src/stores/FirebaseStore.ts
@@ -1,15 +1,27 @@
 import { FirebxRootStore } from "../@firebx-types";
-import { FirebaseApp } from "firebase/app";
+import { FirebaseApp, initializeApp } from "firebase/app";
 import { Firestore, getFirestore } from "firebase/firestore";
+import { Auth, getAuth } from "firebase/auth";
+import {Functions, getFunctions} from 'firebase/functions';
 
 export class FirebaseStore {
   readonly rootStore: FirebxRootStore;
 
+  readonly firebaseApp: FirebaseApp;
+
   readonly firestore: Firestore;
 
-  constructor({ rootStore, firebaseApp }: { rootStore: FirebxRootStore, firebaseApp: FirebaseApp }) {
+  readonly auth: Auth;
+
+  readonly functions: Functions;
+
+  constructor({ rootStore, firebaseConfig }: { rootStore: FirebxRootStore, firebaseConfig: any }) {
     this.rootStore = rootStore;
-    this.firestore = getFirestore(firebaseApp);
+    this.firebaseApp = initializeApp(firebaseConfig);
+    this.firestore = getFirestore(this.firebaseApp);
+    this.auth = getAuth(this.firebaseApp);
+    this.functions = getFunctions(this.firebaseApp);
   }
 
 }
+
